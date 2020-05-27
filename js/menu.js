95a5d148ddcbfc7dec530f4d67cdf8f4c9f68943
Thales Ernane de Souza <talesernani@gmail.com>
2020-05-27 12:16:15 -0300

    Finalizando Projeto Responsivo

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,26 @@
-document.querySelector('.menu-controle').onclick = function() {
-	document.documentElement.classList.toggle('menu-ativo');
+document.querySelector('.menu-abrir').onclick = function() {
+    document.documentElement.classList.add('menu-ativo');
 };
+
+document.querySelector('.menu-fechar').onclick = function() {
+    document.documentElement.classList.remove('menu-ativo');
+};
+
+/** 
+ * 
+ * Estamos usando JavaScript puro no exemplo, mas fique livre para usar algum framework como jQuery.
+
+O JS é bem simples: adiciona eventos de clique nos dois botões que adicionam/removem uma classe menu-ativo no elemento <html> (acessado em JS via document.documentElement).
+
+(se quiser testar já agora, abra o Console no seu navegador e clique nos botões enquanto observa a tag <html> recebendo/removendo a classe).
+*/
+
+document.documentElement.onclick = function(event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
+};
+
+/*
+E ainda podemos incrementar o JavaScript para permitir que o usuário clique na área cinza de fundo para fechar o menu.
+**/
